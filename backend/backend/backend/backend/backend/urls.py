b7c1158cef279e1d12620ecabadc01a361de7099
Poovarasan B <varasanpoo831@gmail.com>
2026-07-08 11:42:45 +0530

    Update frontend/backend assets and config

--- backend/backend/backend/backend/backend/urls.py
+++ backend/backend/backend/backend/backend/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView, RedirectView
 from django.contrib.auth.views import LogoutView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from rest_framework_simplejwt.views import TokenRefreshView
 from users.views import FrontendTokenObtainPairView
 
@@ -36,3 +37,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    ]
